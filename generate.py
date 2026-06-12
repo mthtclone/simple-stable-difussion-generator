d6596590b551fa3f2169415fb7221b0bcb197d32
mthtclone <morrg25@gmail.com>
2026-06-12 09:17:49 +0630

    CUDA version with 40 inference steps

--- generate.py
+++ generate.py
@@ -7,18 +7,28 @@ model_id = "runwayml/stable-diffusion-v1-5"
 
 print("Loading model...")
 
+if torch.cuda.is_available():
+    device = "cuda"
+    dtype = torch.float16
+    print(f"GPU detected: {torch.cuda.get_device_name(0)}")
+else:
+    device = "cpu"
+    dtype = torch.float32
+    print("No GPU detected. Using CPU.")
+
 pipe = StableDiffusionPipeline.from_pretrained(
     model_id,
-    torch_dtype=torch.float32,
+    torch_dtype=dtype,
     cache_dir="./models"
 )
 
-pipe = pipe.to("cpu")
+pipe = pipe.to(device)
 
-print("Model loaded")
+print(f"Model loaded on {device.upper()}")
 
 def generate_image(prompt, controller=None, logger=print):
 
+    logger(f"Using device: {device.upper()}\n")
     logger("Starting generation...\n")
 
     start = time.perf_counter()
@@ -32,12 +42,12 @@ def generate_image(prompt, controller=None, logger=print):
             logger("\nCancel requested. Stopping early...\n")
             raise Exception("Generation cancelled")
 
-        logger(f"Step {step + 1}/20\n")
+        logger(f"Step {step + 1}/80\n")
 
     try:
         image = pipe(
             prompt,
-            num_inference_steps=20,
+            num_inference_steps=40,
             callback=callback,
             callback_steps=1
         ).images[0]
--- run.bat
+++ run.bat
@@ -6,19 +6,19 @@ echo Setting up environment
 echo =====================================
 
 if not exist "sd-venv\Scripts\activate.bat" (
-    python -m venv %VENV_DIR%
+    python -m venv sd-venv
 )
 
 echo Activate virtual environment
-call %VENV_DIR%\Scripts\activate
+call sd-venv\Scripts\activate
 
 echo.
 echo Upgrading pip...
 python -m pip install --upgrade pip
 
 echo.
-echo Installing PyTorch (CPU build)...
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+echo Installing PyTorch (CUDA build)...
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 
 echo.
 echo Installing other requirements...
